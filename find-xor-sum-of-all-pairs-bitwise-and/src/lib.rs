c24ddc18ca8633128c651b167e091cc05757898c
alpoi-x <angus.pointer@xapien.com>
2025-01-03 15:08:25 +0000

    more of an algebra problem than a programming problem but oh well, 84/84

--- find-xor-sum-of-all-pairs-bitwise-and/src/lib.rs
+++ find-xor-sum-of-all-pairs-bitwise-and/src/lib.rs
@@ -1,11 +1,24 @@
 fn get_xor_sum(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
-    let mut sum = 0;
+    /*
+        x = [x_0, ..., x_n]
+        y = [y_0, ..., y_m]
+        s = (x_0 & y_0) ^ (x_0 & y_1) ^ ... ^ (x_i & y_j) ^ ... (x_n & y_m)
+          = (x_0 & (y_0 ^ ... ^ y_m)) ^ ... ^ (x_n & (y_0 ^ ... ^ y_m))
+          = (x_0 ^ ... ^ x_n) & (y_0 ^ ... ^ y_m)
+    */
+
+    let mut x_sum = 0;
+    let mut y_sum = 0;
+
     for i in &arr1 {
-        for j in &arr2 {
-            sum ^= i & j;
-        }
+        x_sum ^= i
     }
-    return sum;
+
+    for j in &arr2 {
+        y_sum ^= j
+    }
+
+    return x_sum & y_sum;
 }
 
 #[cfg(test)]
